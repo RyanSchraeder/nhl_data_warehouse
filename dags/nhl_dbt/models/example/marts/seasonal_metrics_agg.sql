329baabfde1239521153c5907147a9478875d3a2
RyanSchraeder <rschraeder2@gmail.com>
2025-03-18 16:17:27 -0600

    add metrics dbt model

--- dags/nhl_dbt/models/example/marts/seasonal_metrics_agg.sql
+++ dags/nhl_dbt/models/example/marts/seasonal_metrics_agg.sql
@@ -5,14 +5,60 @@
 }}
 
 with src1 as (select * from {{ ref('games') }}), 
-    src2 as (select * from {{ ref('teams') }} )
+    src2 as (select * from {{ ref('teams') }})
 
-select * 
-from src1 
-join src2 
-    on src1.visitor = sr2.team
+(
+    select 
+        GAME_DATE,
+        GAME_TIME,
+        VISITOR,
+        VISITOR_GOALS,
+        HOME,
+        HOME_GOALS,
+        GUESTS_IN_ATTENDANCE,
+        LENGTH_OF_GAME,
+        GP,
+        OVERALL_WINS,
+        OVERALL_LOSSES,
+        OVERTIME_LOSSES,
+        TOTAL_POINTS,
+        POINTS_PERCENTAGE,
+        GOALS_FOR,
+        GOALS_AGAINST,
+        HOCKEY_REFERENCE_SRS,
+        STRENGTH_OF_SCHEDULE,
+        POINTS_PERCENTAGE_IN_REGULATION,
+        WINS_IN_REGULATION,
+        REGULATION_RECORD
+    from src1 
+    join src2 
+        on src1.visitor = src2.team
+)
 union 
-select * 
-from src1 
-join src2 
-    on src1.home = sr2.team
+(
+    select 
+        GAME_DATE,
+        GAME_TIME,
+        VISITOR,
+        VISITOR_GOALS,
+        HOME,
+        HOME_GOALS,
+        GUESTS_IN_ATTENDANCE,
+        LENGTH_OF_GAME,
+        GP,
+        OVERALL_WINS,
+        OVERALL_LOSSES,
+        OVERTIME_LOSSES,
+        TOTAL_POINTS,
+        POINTS_PERCENTAGE,
+        GOALS_FOR,
+        GOALS_AGAINST,
+        HOCKEY_REFERENCE_SRS,
+        STRENGTH_OF_SCHEDULE,
+        POINTS_PERCENTAGE_IN_REGULATION,
+        WINS_IN_REGULATION,
+        REGULATION_RECORD
+    from src1 
+    join src2 
+    on src1.home = src2.team
+)
